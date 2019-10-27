1971dc9a7438f2b9e0ec18a4d23e0e7be87860b0
Ben Fowlie <bsfowlie@gmail.com>
2019-10-27 15:17:29 -0400

    Provides the Position class with updated toString implementation

--- com.github.bsfowlie.bendoku.engine/src/main/java/com/github/bsfowlie/bendoku/engine/Position.java
+++ com.github.bsfowlie.bendoku.engine/src/main/java/com/github/bsfowlie/bendoku/engine/Position.java
@@ -1,12 +1,22 @@
 package com.github.bsfowlie.bendoku.engine;
 
+import java.util.Arrays;
+import java.util.List;
+
 public final class Position {
-    public static Position at(int row, int column) {
-        return new Position();
+
+    private final String representation;
+
+    private Position(final String format) {
+        representation = format;
+    }
+
+    public static Position at(final int row, final int column) {
+        return new Position("(" + row + ", " + column + ")");
     }
 
     @Override
     public String toString() {
-        return "(0, 1)";
+        return representation;
     }
 }
--- com.github.bsfowlie.bendoku.engine/src/test/java/com/github/bsfowlie/bendoku/engine/PositionShould.java
+++ com.github.bsfowlie.bendoku.engine/src/test/java/com/github/bsfowlie/bendoku/engine/PositionShould.java
@@ -9,8 +9,14 @@ public final class PositionShould implements WithAssertions {
         assertThat(Position.at(1,1)).isNotNull();
 
     }
+
     @Test
     public void have_representation_with_row_and_column() {
         assertThat(Position.at(0,1)).hasToString("(0, 1)");
+    }
+
+    @Test
+    public void have_representation_with_correct_row_and_column() {
+        assertThat(Position.at(3,6)).hasToString("(3, 6)");
     }
 }
