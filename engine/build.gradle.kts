600b24631b4fd866360d31a92fbaa590a952d1c3
Ben Fowlie <bsfowlie@gmail.com>
2025-11-27 17:22:16 -0500

    build: add the Assertj library for assertions

--- engine/build.gradle.kts
+++ engine/build.gradle.kts
@@ -7,6 +7,9 @@ repositories {
 }
 
 dependencies {
+    // Use Assertj for assertions
+    testImplementation(libs.assertj.core)
+
     // Use JUnit Jupiter for testing.
     testImplementation(libs.junit.jupiter)
 
