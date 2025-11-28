3856ee00a2073b917a91453e95dc2a8db4172946
Ben Fowlie <bsfowlie@gmail.com>
2025-11-28 11:14:11 -0500

    feat: add a SolvedCell

--- engine/src/main/java/com/github/bsfowlie/bendoku/puzzle/Cell.java
+++ engine/src/main/java/com/github/bsfowlie/bendoku/puzzle/Cell.java
@@ -1,6 +1,6 @@
 package com.github.bsfowlie.bendoku.puzzle;
 
-public abstract sealed class Cell permits EmptyCell, GivenCell {
+public abstract sealed class Cell permits EmptyCell, GivenCell, SolvedCell {
 
    public abstract boolean isSolved();
 
---
+++ engine/src/main/java/com/github/bsfowlie/bendoku/puzzle/SolvedCell.java
@@ -0,0 +1,16 @@
+package com.github.bsfowlie.bendoku.puzzle;
+
+final class SolvedCell extends Cell {
+
+  public SolvedCell(final int value) {
+
+    super();
+  }
+
+  @Override
+  public boolean isSolved() {
+
+    return true;
+  }
+
+}
