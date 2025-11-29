2b6f5fd44b1f30a6ac16d5e61c82f318e2c99132
Ben Fowlie <bsfowlie@gmail.com>
2025-11-28 19:48:11 -0500

    feat: GivenCells have a value

--- engine/src/main/java/com/github/bsfowlie/bendoku/puzzle/GivenCell.java
+++ engine/src/main/java/com/github/bsfowlie/bendoku/puzzle/GivenCell.java
@@ -2,9 +2,12 @@ package com.github.bsfowlie.bendoku.puzzle;
 
 final class GivenCell extends Cell {
 
+  private final int value;
+
   public GivenCell(final int value) {
 
     super();
+    this.value = value;
   }
 
   @Override
@@ -16,7 +19,7 @@ final class GivenCell extends Cell {
   @Override
   public Integer value() {
 
-    return 1;
+    return value;
   }
 
 }
--- engine/src/test/java/com/github/bsfowlie/bendoku/puzzle/GivenCellTest.java
+++ engine/src/test/java/com/github/bsfowlie/bendoku/puzzle/GivenCellTest.java
@@ -3,6 +3,8 @@ package com.github.bsfowlie.bendoku.puzzle;
 import org.assertj.core.api.WithAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("A GivenCell")
 class GivenCellTest implements WithAssertions {
@@ -22,18 +24,18 @@ class GivenCellTest implements WithAssertions {
 
   }
 
-  @Test
-  @DisplayName("should have a value")
-  void shouldHaveValue() {
+  @ParameterizedTest(name = "should have value {0}")
+  @ValueSource(ints = {1, 2, 3, 4})
+  void shouldHaveValue(final int value) {
 
     // given
-    var cell = new GivenCell(1);
+    var cell = new GivenCell(value);
 
     // when
-    var value = cell.value();
+    var actual = cell.value();
 
     // then
-    assertThat(value).isEqualTo(1);
+    assertThat(actual).isEqualTo(value);
 
   }
 
